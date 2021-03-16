b33d8af4bfb354406abdb138e6bc53c76a147c5a
ClaudiaLie <claudiss.lai@gmail.com>
2021-03-16 21:34:52 +0000

    timer now running

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,11 +50,67 @@ function loop() {
 loop();
 
 // Typing Game 1
+window.addEventListener('load', init);
 
 const randomQuotesApi = "https://api.quotable.io/random";
 const quoteDisplayElement = document.getElementById("quoteDisplay");
 const quoteInputElement = document.getElementById("quoteInput");
 // Insert timer here?
+// Typing Game 2 Ref: https://github.com/bradtraversy/wordbeater
+
+//DOM
+const scoreDisplay = document.querySelector("#score");
+const timeDisplay = document.querySelector("#time");
+const message = document.querySelector("#message");
+
+// Globals
+let time = 20;
+let score = 0;
+let isPlaying;
+
+// Initialize Game
+function init() {
+    // Start matching on word input
+    quoteInputElement.addEventListener('input', startMatch);
+    // Call countdown
+    setInterval(countdown, 1000);
+    // Check game status
+    setInterval(checkStatus, 50);
+} 
+
+// Start match
+function startMatch() {
+    if (matchWords()) {
+        isPlaying = true;
+        time = 10;
+        showWord(words);
+        quoteInputElement.value = "";
+        score++;
+        }
+        scoreDisplay.innerHTML = score;
+    }
+
+// Countdown timer
+function countdown() {
+    // Make sure the time has not run out
+    if(time>0){
+        // Decrement
+        time--;
+    } else if(time === 0){
+        //Game Over
+        isPlaying = false;
+    }
+    //Show time
+    timeDisplay.innerHTML = time;
+}
+
+// Check game status
+function checkStatus() {
+    if(!isPlaying && time === 0){
+        message.innerHTML = "Game Over!";
+        score = 0;
+    }
+}
 
 // Compare each character typed to the quote displayed
 quoteInputElement.addEventListener("input", () => {
@@ -103,64 +159,3 @@ async function getNextQuote() {
 
 getNextQuote();
 
-
-// Typing Game 2 Ref: https://github.com/bradtraversy/wordbeater
-
-window.addEventListener("load",init);
-
-// Globals
-let time = 10;
-let score = 0;
-let isPlaying;
-
-//DOM
-const wordInput = document.querySelector("#word-input");
-const currentWord = document.querySelector("#current-word");
-const scoreDisplay = document.querySelector("#score");
-const timeDisplay = document.querySelector("#time");
-const message = document.querySelector("#message");
-
-
-// Initialize Game
-function init() {
-    // Start matching on word input
-    wordInput.addEventListener('input', startMatch);
-    // Call countdown
-    setInterval(countdown, 1000);
-    // Check game status
-    setInterval(checkStatus, 50);
-} 
-
-// Start match
-function startMatch() {
-    if (matchWords()) {
-        isPlaying = true;
-        time = 10;
-        showWord(words);
-        wordInput.value = "";
-        score++;
-        }
-        scoreDisplay.innerHTML = score;
-    }
-
-// Countdown timer
-function countdown() {
-    // Make sure the time has not run out
-    if(time>0){
-        // Decrement
-        time--;
-    } else if(time === 0){
-        //Game Over
-        isPlaying = false;
-    }
-    //Show time
-    timeDisplay.innerHTML = time;
-}
-
-// Check game status
-function checkStatus() {
-    if(!isPlaying && time === 0){
-        message.innerHTML = "Game Over!";
-        score = 0;
-    }
-}
